9491007a35a35b3cdfcd7dc88efa227bf9717213
Caleb J. Yu <calebyu33@gmail.com>
2018-11-10 13:05:24 -0800

    tutorials followed up to end of part 3

--- server/BarBeerDrinker/config.py
+++
@@ -1,2 +0,0 @@
-database_uri = "mysql+pymsql://arjunbansil:databasestudent@jessesdatabase.c1rottfuq1cw.us-east-2.rds.amazonaws.com"
-
--- website/TransactionBarBeerDrinkerUI/src/app/app.module.ts
+++ website/TransactionBarBeerDrinkerUI/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
