8116fc8bd3788b1f6be7887c77731011103b68cf
ArjunBansil <bansilarjun@gmail.com>
2018-11-15 18:03:07 -0500

    added dates to bills

--- dataMaker.py
+++ dataMaker.py
@@ -113,30 +113,37 @@ def test():
 	transactsRelation = []
 	printedOnRelation = []
 	count = 0
+	dateData = pd.read_csv("random_date.csv")
+	rDates = dateData['date']
+
+	
 	for ID in originalBills['trans_id']:
 		#choose a bar, choose a drinker, choose how many items and which items, find price
 		barNum = randint(0,len(barNames)-1)
 		drinkerNum = randint(0,len(drinkerNames)-1)
 		itemNum = randint(1,5)
-		barSells = []
+		randDateNum = randint(0, len(rDates)-1)
+		#barSells = []
 		subtotal = 0
-		for x in sellsRelation:
-			if x[0] is barNames[barNum] and (x[1],x[2]) not in barSells:
-				barSells.append((x[1],x[2]))
+		#for x in sellsRelation:
+			#if x[0] is barNames[barNum] and (x[1],x[2]) not in barSells:
+				#barSells.append((x[1],x[2]))
 		items = []
 		for i in range(itemNum):
-			rand = randint(0,len(barSells)-1)
-			item = barSells[rand][0]
-			items.append(item)
-			price = barSells[rand][1]
+			#rand = randint(0,len(barSells)-1)
+			#item = barSells[rand][0]
+			#items.append(item)
+			#price = barSells[rand][1]
 			subtotal += price
 		time = originalBills['time'][count]
 		count += 1
 		tip = subtotal * 0.15
 		tax = subtotal * 0.06
-		bills.append((ID,time,subtotal,tip,subtotal+tip+tax))
+		date = rDates[randDateNum]
+		bills.append((ID,date,time,subtotal,tip,subtotal+tip+tax))
 		for i in items:
-			printedOnRelation.append((i,ID))
+			if((i,ID) not in printedOnRelation):
+				printedOnRelation.append((i,ID))
 		transactsRelation.append((drinkerNames[drinkerNum],ID,barNames[barNum]))
 
 	operatesRelation = []
@@ -168,6 +175,7 @@ for bar in barNames:
 		a = (day,bar,start,end)
 		if a not in hours:
 			hours.append(a)
+
 
 #output: dataframe
 #print(pd.DataFrame(frequentsRelation))
