8e7c465c89374043dd79bea994636b3ab2c3a4a3
Caleb J. Yu <calebyu33@gmail.com>
2018-11-17 22:16:59 -0800

    .

--- website/TransactionBarBeerDrinkerUI/src/app/bartender-details/bartender-details.component.ts
+++ website/TransactionBarBeerDrinkerUI/src/app/bartender-details/bartender-details.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BartendersService } from '../bartenders.service';
 import { ActivatedRoute } from '@angular/router';
+declare const Highcharts: any;
 
 export interface Shift{
-
+  shift:string;
+  amount_of_beers:number;
 }
 
 @Component({
@@ -23,16 +25,85 @@ export class BartenderDetailsComponent implements OnInit {
   ) {
     route.paramMap.subscribe((paramMap => {
       this.bartenderName = paramMap.get('bartender');
-      //this.barName = ;
-      bartenderService.get_shifts(this.bartenderName, this.barName).subscribe(
-        data => {
-          this.shifts = data;
-        }
-      );
-    }))
+      //this.barName = (document.getElementById("bar_name") as HTMLInputElement).value;
+      if(this.barName != ""){
+        bartenderService.get_shifts(this.bartenderName, this.barName).subscribe(
+          data=>{
+            const shiftss = [];
+            const amounts = [];
+            data.forEach(shift=>{
+              shiftss.push(shift.shift);
+              amounts.push(shift.amount_of_beers);
+            });
+            this.renderChart(shiftss, amounts, 'Beers Sold Per Shift', 'Shift', 'Amount of Beer',
+              'bargraph');
+          }
+        );
+      }
+    }));
+
    }
 
   ngOnInit() {
+  }
+  get_shifts(){
+    this.barName = (document.getElementById("bar_name") as HTMLInputElement).value;
+      if(this.barName != ""){
+        this.bartenderService.get_shifts(this.bartenderName, this.barName).subscribe(
+          data=>{
+            const shiftss = [];
+            const amounts = [];
+            data.forEach(shift=>{
+              shiftss.push(shift.shift);
+              amounts.push(shift.amount_of_beers);
+            });
+            this.renderChart(shiftss, amounts, 'Beers Sold Per Shift', 'Shift', 'Amount of Beer',
+              'bargraph');
+          }
+        );
+      }
+  }
+  renderChart(xData: string[] , yData: number[], title:string, x:string, y:string ,id:string){
+    Highcharts.chart(id, {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: title
+      },
+      xAxis: {
+        categories: xData,
+        title: {
+          text: x
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: y
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        data: yData
+      }]
+    });
+
   }
 
 }
